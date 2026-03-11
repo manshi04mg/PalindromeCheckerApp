98d9d68d0e1e238188c894bef6f4374e05eaea7d
manshi04mg <manshi.gupta.mg2006@gmail.com>
2026-03-11 21:35:13 +0530

    uc9

--- App/src/PalidromeCheckerApp.java
+++ App/src/PalidromeCheckerApp.java
@@ -1,83 +1,33 @@
 public class PalidromeCheckerApp {
 
-    // Node class for singly linked list
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
+    // Recursive method
+    public static boolean isPalindrome(String str, int start, int end)
+    {
+        // Base condition
+        if(start >= end)
+        {
+            return true;
         }
+
+        // If characters don't match
+        if(str.charAt(start) != str.charAt(end))
+        {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
 
         String word = "madam";
 
-        // Convert string to linked list
-        Node head = null;
-        Node tail = null;
-
-        for(int i = 0; i < word.length(); i++)
-        {
-            Node newNode = new Node(word.charAt(i));
-
-            if(head == null)
-            {
-                head = newNode;
-                tail = newNode;
-            }
-            else
-            {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        // Fast and slow pointer to find middle
-        Node slow = head;
-        Node fast = head;
-
-        while(fast != null && fast.next != null)
-        {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Reverse second half
-        Node prev = null;
-        Node current = slow;
-        Node next = null;
-
-        while(current != null)
-        {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        // Compare first and second half
-        Node first = head;
-        Node second = prev;
-
-        boolean isPalindrome = true;
-
-        while(second != null)
-        {
-            if(first.data != second.data)
-            {
-                isPalindrome = false;
-                break;
-            }
-
-            first = first.next;
-            second = second.next;
-        }
+        // Call recursive function
+        boolean result = isPalindrome(word, 0, word.length()-1);
 
         // Display result
-        if(isPalindrome)
+        if(result)
         {
             System.out.println(word + " is a Palindrome");
         }
